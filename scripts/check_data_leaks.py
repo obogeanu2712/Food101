c6e6b18ed81e558f96fcc05804eb0b4a4c5be199
obogeanu <obogeanumateirazvan@gmail.com>
2026-03-19 23:56:08 +0200

    restart

--- scripts/check_data_leaks.py
+++
@@ -1,7 +0,0 @@
-with open("./meta/train_curated.txt") as f1, open("./meta/test_curated.txt") as f2:
-    common = set(f1.read().splitlines()) & set(f2.read().splitlines())
-
-for line in common:
-    print(line)
-
-print(bool(common))
